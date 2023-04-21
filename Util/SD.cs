c606bd2b3a0a548c9ad0503352c50092022f5b27
AnshulBanndhuCs <130958183+AnshulBanndhuCs@users.noreply.github.com>
2023-04-21 14:07:01 +0530

    Add files via upload

---
+++ Util/SD.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore_Auth_Backend_API_Utility.Util
+{
+    public class SD
+    {
+        public const string Role_Admin = "Admin";
+        public const string Role_User = "User";
+    }
+}
